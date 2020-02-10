24202e83c0f1be4dc2866f5938b69f7e83f3d0f3
Miguel Grinberg <miguel.grinberg@gmail.com>
2020-02-10 15:02:57 +0000

    flask backend

---
+++ api/api.py
@@ -0,0 +1,9 @@
+import time
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/time')
+def get_current_time():
+    return {'time': time.time()}
--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [currentTime, setCurrentTime] = useState(0);
+
+  useEffect(() => {
+    fetch('/time').then(res => res.json()).then(data => {
+      setCurrentTime(data.time);
+    });
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
@@ -18,6 +26,7 @@ function App() {
         >
           Learn React
         </a>
+        <p>The current time is {currentTime}.</p>
       </header>
     </div>
   );
