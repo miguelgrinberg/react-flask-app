048642ef072ede4858d1c318bf677006f7a81a08
Miguel Grinberg <miguel.grinberg@gmail.com>
2020-03-16 14:59:21 +0000

    deployment changes

--- api/api.py
+++ api/api.py
@@ -1,9 +1,14 @@
 import time
 from flask import Flask
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='../build', static_url_path='/')
 
 
-@app.route('/time')
+@app.route('/')
+def index():
+    return app.send_static_file('index.html')
+
+
+@app.route('/api/time')
 def get_current_time():
     return {'time': time.time()}
--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   const [currentTime, setCurrentTime] = useState(0);
 
   useEffect(() => {
-    fetch('/time').then(res => res.json()).then(data => {
+    fetch('/api/time').then(res => res.json()).then(data => {
       setCurrentTime(data.time);
     });
   }, []);
