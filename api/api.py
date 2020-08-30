28cfe6b87124501685541027d15ca422c53f293e
Miguel Grinberg <miguel.grinberg@gmail.com>
2020-08-30 16:02:55 +0100

    add client-side routing

--- api/api.py
+++ api/api.py
@@ -4,8 +4,13 @@ from flask import Flask
 app = Flask(__name__, static_folder='../build', static_url_path='/')
 
 
+@app.errorhandler(404)
+def not_found(e):
+    return app.send_static_file('index.html')
+
+
 @app.route('/')
-def index():
+def index(path):
     return app.send_static_file('index.html')
 
 
