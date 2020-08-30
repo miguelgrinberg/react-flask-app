37c821ba37fbd1350c789408e8fbcfde58b113e1
Miguel Grinberg <miguel.grinberg@gmail.com>
2020-08-30 16:02:55 +0100

    add client-side routing

--- api/api.py
+++ api/api.py
@@ -4,6 +4,11 @@ from flask import Flask
 app = Flask(__name__, static_folder='../build', static_url_path='/')
 
 
+@app.errorhandler(404)
+def not_found(e):
+    return app.send_static_file('index.html')
+
+
 @app.route('/')
 def index():
     return app.send_static_file('index.html')
--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import { BrowserRouter, Link, Switch, Route } from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
 
@@ -14,19 +15,33 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <p>The current time is {currentTime}.</p>
+        <BrowserRouter>
+          <div>
+            <Link className="App-link" to="/">Home</Link>
+            &nbsp;|&nbsp;
+            <Link className="App-link" to="/page2">Page2</Link>
+          </div>
+          <Switch>
+            <Route exact path="/">
+                <img src={logo} className="App-logo" alt="logo" />
+                <p>
+                  Edit <code>src/App.js</code> and save to reload.
+                </p>
+                <a
+                  className="App-link"
+                  href="https://reactjs.org"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Learn React
+                </a>
+                <p>The current time is {currentTime}.</p>
+            </Route>
+            <Route path="/page2">
+                <p>This is page 2!</p>
+            </Route>
+          </Switch>
+        </BrowserRouter>
       </header>
     </div>
   );
