ca70f2f1d9218f7f45875f86cd340d9f034dec63
nfeagh <114145380+nfeagh@users.noreply.github.com>
2023-05-23 23:00:19 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -25,7 +25,7 @@ function App() {
             <Route exact path="/">
                 <img src={logo} className="App-logo" alt="logo" />
                 <p>
-                  Edit <code>src/App.js</code> and save to reload. This is a change
+                  Edit <code>src/App.js</code> and save to reload. I hosted this, you figure the rest out :)
                 </p>
                 <a
                   className="App-link"
