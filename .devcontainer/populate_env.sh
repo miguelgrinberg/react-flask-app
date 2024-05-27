506e0ee38bc0b62536082d26daa427770c41797a
KushTurner <kushturner9@gmail.com>
2024-05-27 14:00:12 +0100

    Fixed start up script

--- .devcontainer/populate_env.sh
+++ .devcontainer/populate_env.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-cd ..
+# .env needs to be in the root folder
 
-if [ ! -f ".env" ]; then
-  touch ".env"
-fi
+touch .env
 
 echo "VITE_CODESPACE_NAME=https://${CODESPACE_NAME}-3000.app.github.dev" >> .env
 echo "VITE_GITHUB_TOKEN=${GITHUB_TOKEN}" >> .env
+
+echo "Populated .env with Github codespaces essentials"
 
