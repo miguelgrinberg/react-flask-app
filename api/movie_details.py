2c427bbec49d30f38421e0d69d98179a9f7e8523
Tushita Yadav <72255751+yadavtushita@users.noreply.github.com>
2024-06-20 19:31:51 +0000

    Add stetch api task and remove unused file

--- api/movie_details.py
+++
@@ -1,6 +0,0 @@
-import requests
-
-def get_movies_by_id(searchValue):
-    url = "http://www.omdbapi.com/?i=" + str(searchValue) + "&apikey=2651b0db"
-    response = requests.get(url=url)
-    return response.json()
