2b063d593ec909a812b13d7a108bbfe0937ad5a1
philyeung <55381380+philyeung@users.noreply.github.com>
2024-06-21 08:01:51 +0000

    Add correct spelling

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,7 +2,7 @@ import useMoviesContext from "../context/MoviesContext";
 
 export const Tabs = Object.freeze({
   Movies: "Movies",
-  Favourites: "Favorits",
+  Favourites: "Favourites",
   Recommended: "Recommended",
 });
 
